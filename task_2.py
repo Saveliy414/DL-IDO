47adef23209a1ab9deaad1a77a9421c70c89260c
Saveliy414 <160126898+Saveliy414@users.noreply.github.com>
2024-11-06 03:37:37 +0300

    Updated task_2.py

--- task_2.py
+++ task_2.py
@@ -1,12 +1,13 @@
 # TODO Напишите функцию find_common_participants
 
 def find_common_participants(participant_1, participant_2, separator=','):
-    result = []
+    result = list()
     splited_participant_1 = participant_1.split(separator)
     splited_participant_2 = participant_2.split(separator)
     for name in splited_participant_1:
-        if splited_participant_2.__contains__(name):
+        if name in splited_participant_2:
             result.append(name)
+    result.sort()
     return result
 
 
@@ -15,5 +16,4 @@ participants_second_group = "Петров|Сидоров|Смирнов"
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
 common_participants = find_common_participants(participants_first_group, participants_second_group)
-common_participants.sort()
 print(common_participants)
